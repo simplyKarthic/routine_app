4c85d30721abc70fc341ea01d845cf108ccb233a
karthic <karthicjayaraj@gmail.com>
2022-08-14 21:28:24 +0530

    category create and update added

---
+++ lib/collections/routine.dart
@@ -0,0 +1,21 @@
+import 'package:isar/isar.dart';
+import 'package:routine_app/collections/category.dart';
+part 'routine.g.dart';
+
+@Collection()
+class Routine{
+
+  @Id()
+  int id = Isar.autoIncrement;
+
+  late String title;
+
+  @Index()
+  late DateTime startTime;
+
+  @Index(caseSensitive: false)
+  late String day;
+
+  @Index(composite: [CompositeIndex('title')])
+  final category = IsarLink<Category>();
+}
---
+++ lib/main.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:isar/isar.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:routine_app/collections/category.dart';
+import 'package:routine_app/collections/routine.dart';
+import 'package:routine_app/screens/create_routine.dart';
+
+void main() async {
+  final dir = await getApplicationSupportDirectory();
+  final isar = await Isar.open(
+    schemas: [RoutineSchema, CategorySchema],
+    directory: dir.path
+  );
+
+  runApp(MyApp(isar: isar));
+}
+
+class MyApp extends StatelessWidget {
+  final Isar isar;
+  const MyApp({Key? key, required this.isar}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Routine app',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MainPage(isar: isar),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class MainPage extends StatefulWidget {
+  final Isar isar;
+  const MainPage({Key? key, required this.isar}) : super(key: key);
+
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Routine"),
+        actions: [
+          IconButton(
+              onPressed: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => CreateRoutine(isar: widget.isar)));
+              },
+              icon: const Icon(Icons.add))
+        ],
+      ),
+    );
+  }
+}
